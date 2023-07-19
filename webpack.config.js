f7f7c2d98fe1fae618dca927fc02800c622b8d47
Audrey Tesson <audrey.tesson.pro@outlook.fr>
2023-07-19 11:07:18 +0200

    css removed, tailwindcss installed, templates with tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,8 @@ Encore
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
 
+    .enablePostCssLoader()
+
     /*
      * FEATURE CONFIG
      *
