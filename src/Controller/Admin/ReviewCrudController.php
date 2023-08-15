47266b28eab76aa6ec870ff3664c768fe68c6254
Audrey Tesson <audrey.tesson.pro@outlook.fr>
2023-08-15 22:31:47 +0200

    rating crud already added with last push, fields done

--- src/Controller/Admin/ReviewCrudController.php
+++ src/Controller/Admin/ReviewCrudController.php
@@ -4,7 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Review;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 
 class ReviewCrudController extends AbstractCrudController
 {
@@ -15,11 +18,11 @@ class ReviewCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-            // IdField::new('id'),
-            // TextField::new('title'),
-            // TextEditorField::new('description'),
+            yield IdField::new('id');
+            yield TextEditorField::new('content');
+            yield AssociationField::new('username', 'User');
+            yield AssociationField::new('city', 'City');
             yield IntegerField::new('rating', 'Notes')
-            // ->hideOnIndex()
             ->setTemplatePath('admin/field/rating.html.twig')   
             ;
 
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -209,4 +209,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->username;
+    }
 }
